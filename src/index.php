55272b21470751697940ac07eccb6550b409d49d
Andres Toro <carlostoro04@gmail.com>
2020-07-05 00:37:37 -0500

    collect landing page

--- src/index.php
+++ src/index.php
@@ -1,115 +1,132 @@
-<?php
-    session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link 
-        rel="stylesheet" 
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
-        crossorigin="anonymous"
-    >
-    <style>
-        html body {
-            height: 100%;
-            background: rgb(9,9,121);
-            background: linear-gradient(90deg, rgba(9,9,121,1) 0%, rgba(9,9,121,1) 5%, rgba(2,0,36,1) 20%, rgba(41,0,33,1) 80%, rgba(216,0,17,1) 95%, rgba(255,0,14,1) 100%);
-        }
-        h1{
-            text-align: center;
-        }
-        img{
-            width: 40%;
-            height: 40%;
-        }
-        .form-container {
-            padding: 2%;
-            margin: 5%;
-        }
-    </style>
-    <title>Cars Registration</title>
-</head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="Web Application to Manager Your Music Collections" content="">
+  <meta name="CharliToro" content="">
+
+  <title>Collect</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link 
+    rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
+    crossorigin="anonymous"
+  >
+
+  <!-- Custom Fonts -->
+  <link href="styles/fonts.css" rel="stylesheet" type="text/css">
+  <link href="styles/simple-line-icons.css" rel="stylesheet">
+  <link 
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" 
+    rel="stylesheet" 
+    type="text/css"
+  >
+
+  <!-- Custom CSS -->
+  <link href="styles/main.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <?php
-        if( !isset($_SESSION['userId']) ) {
-            header('location: ./pages/login.php');
-            exit();
-        }
-    ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">
-            <img src="https://img.icons8.com/color/96/000000/valet-parking.png"/>
-        </a>
-        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <form class="form-inline mx-right my-2 my-lg-0" action="./plugins/user-logout.php" method="POST">
-                        <button class="btn btn-outline-primary mx-right my-2 my-sm-0" type="submit">
-                            <img src="https://img.icons8.com/android/48/000000/logout-rounded-left.png"/>
-                        </button>
-                    </form>
-                </li>
-            </ul>
+<body id="page-top">
+
+  <!-- Navigation -->
+  <?php include 'components/navigation.php'?>
+
+  <!-- Header -->
+  <header class="masthead d-flex">
+    <div class="container text-center my-auto">
+      <h1 class="mb-1">Collect</h1>
+      <h3 class="mb-5">
+        <em>Platform to Manager Your Music Collections</em>
+      </h3>
+      <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Sign Up</a>
+    </div>
+    <div class="overlay"></div>
+  </header>
+
+  <!-- About -->
+  <section class="content-section bg-light" id="about">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-lg-10 mx-auto">
+          <h2>Collect is a web platform that allows you to manage your music collections that will grow over time</h2>
+          <p class="lead mb-5">You can add your vinyl records, cassettes, CDs and collectibles from your favorite bands and artists
         </div>
-    </nav>
-    <div class="form-container shadow p-3 mb-5 bg-white rounded" >
-        <h1>Parking Cars Registration </h1>
-        <form id="carts-form" >
-            <div class="form-group">
-                <label for="photoLabel">Photo</label>
-                <input type="file" class="form-control-file" name="photo" id="photo">
-            </div>
-            <div class="form-group">
-            <label for="brandLabel">Brand</label>
-            <select class="form-control" name="brand" id="brand">
-                <option>...</option>
-                <option>Mazda</option>
-                <option>Chevrolet</option>
-                <option>Toyota</option>
-                <option>BMW</option>
-                <option>Suzuky</option>
-                <option>Ferrari</option>
-                <option>Mercedes Benz</option>
-                <option>Jeep</option>
-                <option>Other</option>
-            </select>
-            </div>
-            <div class="form-group">
-                <label for="modelLabel">Model</label>
-                <input type="number" class="form-control" class="form-control" name="model" id="model">
-            </div>
-            <div class="form-group">
-                <label for="colorLabel">Color</label>
-                <input type="color" class="form-control" name="color" id="color">
-            </div>
-            <div class="form-group">
-                <label for="licensePlate">License Plate</label>
-                <input type="text" class="form-control" name="licensePlate" id="licensePlate">
-            </div>
-            <div class="form-group">
-                <label for="proprietorNameLabel">Proprietor Name</label>
-                <input type="text" class="form-control" name="proprietorName" id="proprietorName">
-            </div>
-            <div class="form-group">
-                <label for="proprietorLastnameLabel">Proprietor Lastname</label>
-                <input type="text" class="form-control" name="proprietorLastname" id="proprietorLastname">
-            </div>
-            <input type="hidden" name="form_submitted" value="1" />
-            <button type="reset" class="btn btn-primary">Register</button>
-        </form>
+      </div>
     </div>
-    <div class="form-container shadow p-3 mb-5 bg-white rounded">
-        <h1 style='margin-bottom: 5%'>Registered Cars</h1>
-        <div id="carts-list" class='row row-cols-4'></div>
-    </div>
+  </section>
 
-    <!--  -->
-    <script src="index.js"></script>
-    </body>
+  <!-- Services -->
+  <section class="content-section bg-primary text-white text-center" id="services">
+    <div class="container">
+      <div class="content-section-heading">
+        <h2 class="mb-5">What You Can Manage</h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <i class="icon-screen-smartphone"></i>
+          </span>
+          <h4>
+            <strong>Vinyls</strong>
+          </h4>
+          <p class="text-faded mb-0">For audiophiles everywhere, it remains the singular most impressive format of recording and reproducing music</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <i class="icon-pencil"></i>
+          </span>
+          <h4>
+            <strong>Cassettes</strong>
+          </h4>
+          <p class="text-faded mb-0">Today, we may think of cassette tapes as retro and vintage artifacts from funkier, groovier days</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <i class="icon-like"></i>
+          </span>
+          <h4>
+            <strong>CDs</strong>
+          </h4>
+          <p class="text-faded mb-0">With the CD all but dead, see it off in style with this trip down memory lane.</p>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <i class="icon-mustache"></i>
+          </span>
+          <h4>
+            <strong>Collectibles</strong>
+          </h4>
+          <p class="text-faded mb-0">Strange items from your favorite bands will make an invaluable music collection</p>
+        </div>
+      </div>
+    </div>
+  </section> 
+
+  <!-- Footer -->
+  <?php include 'components/footer.php'  ?>
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="js/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/stylish-portfolio.js"></script>
+
 </body>
+
 </html>
